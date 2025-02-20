30a632c16fda53fd52edd8522b1e78bbda70187f
MukhteshPendem <mukhteshpendem31@gmail.com>
2025-02-20 20:40:33 +0530

    Added commit mock data for upcoming Talks and Current talk function verification

--- assets/data.private.js
+++ assets/data.private.js
@@ -4,7 +4,34 @@ class Data {
 
         this.context = context;
     }
+
+    getCurrentTalk(){
+
+        return this.getUpcomingTalks()[0];
+    }
+
+    getUpcomingTalks(){
+
+        return [
+            {
+                title: 'Programmers at Work',
+                code : 'Developers',
+                startTime:"2025-02-20T14:00:00.220Z",
+                speakers:["+916304894373"],
+            },
+            {
+                title: 'Ted talks every tuesday',
+                code : 'TEDTalk',
+                startTime:"2025-02-20T14:00:00.220Z",
+                speakers:["+916304894373"],
+            }
+        ];
+
+    }
+
 }
+
+
 
 
 
--- functions/incoming-call.js
+++ functions/incoming-call.js
@@ -1,10 +1,29 @@
+
+const assets = Runtime.getAssets();
+
+const {Data} = require(assets["/data.js"].path);
+
 exports.handler = function (context,event,callback){
 
 
     const twiml = new Twilio.twiml.VoiceResponse();
 
     //TODO: THIS SHOULD BE THE NAME OF THE CURRENT TALK
-    twiml.dial().conference("sample-conference-name");
+
+    const data = new Data(context);
+
+    const talk = data.getCurrentTalk();
+
+    if (talk !== undefined){
+
+        twiml.dial().conference({muted:false},talk.code);
+    }
+
+    else{
+        twiml.say("There is no talk currently. Send us a text for a schedule of upcoming talks")
+    }
+
+
     console.log(`TWIML is ${twiml}`)
     return callback(null, twiml);
 
